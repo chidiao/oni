589ea25e2aaa74f0b867dcf7218b08d9557d1d98
孙颖洲 <sunyz94123@qq.com>
2026-09-23 18:35:52 +0800

    docs: 更新

--- docs/.vitepress/nav.ts
+++ docs/.vitepress/nav.ts
@@ -1,27 +1,28 @@
-import type { DefaultTheme } from "vitepress";
+import type { DefaultTheme } from 'vitepress'
 
-export const nav: DefaultTheme.Config["nav"] = [
-  { text: "Home", link: "/" },
+export const nav: DefaultTheme.Config['nav'] = [
+  { text: 'Home', link: '/' },
   {
-    text: "指南",
+    text: '指南',
     items: [
-      { text: "基地", link: "/guide/jidi" },
-      { text: "气体", link: "/guide/gas" },
-      { text: "液体", link: "/guide/liquid" },
-      { text: "液门", link: "/guide/door" },
-    ],
+      { text: '基地', link: '/guide/jidi' },
+      { text: '气体', link: '/guide/gas' },
+      { text: '液体', link: '/guide/liquid' },
+      { text: '液门', link: '/guide/door' }
+    ]
   },
   {
-    text: "食物",
-    items: [{ text: "食物总览", link: "/food/index" }],
+    text: '食物',
+    items: [{ text: '总览', link: '/food/index' }]
   },
   {
-    text: "动物",
+    text: '动物',
     items: [
-      { text: "养殖", link: "/animal/index" },
-      { text: "养鱼", link: "/animal/fish" },
-      { text: "壁虎", link: "/animal/gecko" },
-    ],
+      { text: '总览', link: '/animal/index' },
+      { text: '养鱼', link: '/animal/fish' },
+      { text: '壁虎', link: '/animal/gecko' },
+      { text: '哈奇', link: '/animal/haqi' }
+    ]
   },
-  { text: "Links", link: "/links" },
-];
+  { text: 'Links', link: '/links' }
+]
--- docs/.vitepress/sidebar.ts
+++ docs/.vitepress/sidebar.ts
@@ -1,29 +1,30 @@
-import type { DefaultTheme } from "vitepress";
+import type { DefaultTheme } from 'vitepress'
 
-export const sidebar: DefaultTheme.Config["sidebar"] = [
+export const sidebar: DefaultTheme.Config['sidebar'] = [
   {
-    text: "指南",
+    text: '指南',
     items: [
-      { text: "基地", link: "/guide/jidi" },
-      { text: "气体", link: "/guide/gas" },
-      { text: "液体", link: "/guide/liquid" },
-      { text: "液门", link: "/guide/door" },
-    ],
+      { text: '基地', link: '/guide/jidi' },
+      { text: '气体', link: '/guide/gas' },
+      { text: '液体', link: '/guide/liquid' },
+      { text: '液门', link: '/guide/door' }
+    ]
   },
   {
-    text: "食物",
-    items: [{ text: "食物总览", link: "/food/index" }],
+    text: '食物',
+    items: [{ text: '总览', link: '/food/index' }]
   },
   {
-    text: "养殖",
+    text: '养殖',
     items: [
-      { text: "养殖总览", link: "/animal/index" },
-      { text: "养鱼", link: "/animal/fish" },
-      { text: "壁虎", link: "/animal/gecko" },
-    ],
+      { text: '总览', link: '/animal/index' },
+      { text: '养鱼', link: '/animal/fish' },
+      { text: '壁虎', link: '/animal/gecko' },
+      { text: '哈奇', link: '/animal/haqi' }
+    ]
   },
   {
-    text: "其他",
-    items: [{ text: "参考链接", link: "/links" }],
-  },
-];
+    text: '其他',
+    items: [{ text: '参考链接', link: '/links' }]
+  }
+]
